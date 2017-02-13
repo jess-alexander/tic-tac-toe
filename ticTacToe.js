2d00d82542ca11c1dc1abae3567444c0e8128e19
jess-alexander <alexanderjessmarie@gmail.com>
2017-02-13 22:18:06 +0100

    improved documentation

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,20 +1,29 @@
-//player true --> player1
-//player false --> player2
+////////////////////////////////////////////////
+/////////       INITIALIZATION         /////////     
+////////////////////////////////////////////////
+
+//player true --> player1, X
+//player false --> player2, O
 var player = true; //default to player 1 start
 
-var board = [ //init with large un-equal numbers
+var board = [ //init with large un-equal numbers   (not 1 or 2) 
     //update this array with values of 1 or 2
     [10, 20, 30],
     [40, 50, 60],
     [70, 80, 90]
 ];
+
+////////////////////////////////////////////////
+/////////       CLICK-LISTENER         /////////     
+////////////////////////////////////////////////
+
 $("#reset").click(function() {
-    board = [ //init with large un-equal numbers    
+    board = [ //init with large un-equal numbers   (not 1 or 2) 
         [10, 20, 30],
         [40, 50, 60],
         [70, 80, 90]
     ];
-    $(".mark").remove();
+    $(".mark").remove(); // Remove the set of matched elements from the DOM.
 
 });
 
@@ -34,40 +43,53 @@ $("div.item").click(function() {
     }
 });
 
-function updateScoreBoard(id) { //
-    switch (id) {
+////////////////////////////////////////////////
+///////      FUNCTION DEFINITION         ///////   
+////////////////////////////////////////////////
+
+function updateScoreBoard(id) {
+    // sort based on id to array position. Change value in array to 1 or 2 based on player 1 or 2
+    switch (id) { //id is a string
         case '1':
             board[0][0] = checkPlayer();
             console.log("board[0][0]" + board[0][0]);
             break;
+
         case '2':
             board[0][1] = checkPlayer();
             console.log("board[0][1]" + board[0][1]);
             break;
+
         case '3':
             board[0][2] = checkPlayer();
             console.log("board[0][2]" + board[0][2]);
             break;
+
         case '4':
             board[1][0] = checkPlayer();
             console.log("board[1][0]" + board[1][0]);
             break;
+
         case '5':
             board[1][1] = checkPlayer();
             console.log("board[1][1]" + board[1][1]);
             break;
+
         case '6':
             board[1][2] = checkPlayer();
             console.log("board[1][2]" + board[1][2]);
             break;
+
         case '7':
             board[2][0] = checkPlayer();
             console.log("board[2][0]" + board[2][0]);
             break;
+
         case '8':
             board[2][1] = checkPlayer();
             console.log("board[2][1]" + board[2][1]);
             break;
+
         case '9':
             board[2][2] = checkPlayer();
             console.log("board[2][2]" + board[2][2]);
@@ -76,6 +98,7 @@ function updateScoreBoard(id) { //
 }
 
 function checkPlayer() {
+    // player's scope is kept open for this purpose
     if (player) {
         console.log("saved for X");
         return 1;
@@ -87,7 +110,6 @@ function checkPlayer() {
 
 function checkForWin() {
     // there's a problem here, only X is declared the winner.
-    console.log("checking for winner");
     if ((board[0][0] == board[0][1] == board[0][2]) ||
         (board[1][0] == board[1][1] == board[1][2]) ||
         (board[2][0] == board[2][1] == board[2][2]) ||
@@ -99,12 +121,12 @@ function checkForWin() {
         (board[0][0] == board[1][1] == board[2][2]) ||
         (board[0][2] == board[1][1] == board[2][0])) {
         // ^ diagonal line wins
-        return true;
+        return true; //winner winner!
     } else {
-        return false;
+        return false; //keep playing
     }
 }
-
+// visualization of array locations
 //  0.0 |  0.1  |  0.2
 //  1.0 |  1.1  |  1.2
 //  2.0 |  2.1  |  2.2
