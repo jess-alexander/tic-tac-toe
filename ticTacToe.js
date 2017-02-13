eb5659789449103977282cafc83bf396698e2960
jess-alexander <alexanderjessmarie@gmail.com>
2017-02-13 15:13:13 +0100

    add in board logic (x and o)

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,13 @@
+//player true --> player1
+//player false --> player2
+var player = true;
 
+$(".item").click(function() {
+    if (player) {
+        $(this).text("X");
+    } else {
+        $(this).text("O");
+    }
+    player = !player;
+
+});
